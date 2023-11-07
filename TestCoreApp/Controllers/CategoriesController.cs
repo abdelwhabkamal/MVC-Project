a8fff028c3a1f8953e6bf3f883f7deec7324fd8e
abdelwhabkamal <abdewhabkamal17@gmail.com>
2023-11-07 20:09:17 +0200

    Creation Categories Controller

---
+++ TestCoreApp/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCoreApp.Models;
+using TestCoreApp.Repository.Base;
+
+namespace TestCoreApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private IRepository<Category> _categoryRepository;
+        public CategoriesController(IRepository<Category> repository ) {
+            _categoryRepository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View(_categoryRepository.GetAll());
+        }
+    }
+}
