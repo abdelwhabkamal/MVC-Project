85daafdd21e82ba749fef54464507bc95642a993
abdelwhabkamal <abdewhabkamal17@gmail.com>
2023-11-07 18:59:43 +0200

    Creation Generic Repository

---
+++ TestCoreApp/Repository/Base/IRepository.cs
@@ -0,0 +1,9 @@
+﻿// Creation of base generic Repository Interface 
+namespace TestCoreApp.Repository.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
---
+++ TestCoreApp/Repository/MainRepository.cs
@@ -0,0 +1,24 @@
+﻿// Creation of Main Repository class
+using TestCoreApp.Data;
+using TestCoreApp.Repository.Base;
+
+namespace TestCoreApp.Repository
+{
+    public class MainRepository<T>:IRepository<T> where T : class
+    {
+        private AppDbContext _context;
+        public MainRepository(AppDbContext context) {
+            this._context = context;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+    }
+}
