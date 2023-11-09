0ca5c7f9d6a15212b403448a30f19565a9d86f8f
abdelwhabkamal <abdewhabkamal17@gmail.com>
2023-11-09 21:54:35 +0200

    Create and Implement Unit Of Work Pattern

---
+++ TestCoreApp/Repository/Base/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿// Creation of base Unit Of Work Pattern Interface 
+
+using Elfie.Serialization;
+using TestCoreApp.Models;
+
+namespace TestCoreApp.Repository.Base
+{
+  //IDisposable helps prevent resource leaks and ensures efficient resource management
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Category> categories {  get; }
+        IRepository<Employee> employees { get; }
+        IRepository<Item> items { get; }
+        int CommitChanges();
+    }
+}
---
+++ TestCoreApp/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿// Implmentation of Unit Of Work Pattern
+
+using TestCoreApp.Data;
+using TestCoreApp.Models;
+using TestCoreApp.Repository.Base;
+
+namespace TestCoreApp.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(AppDbContext context ) {
+            _context = context;
+            categories=new MainRepository<Category>(_context);
+            items=new MainRepository<Item>(_context);
+            employees=new MainRepository<Employee>(_context);
+        }
+        private AppDbContext _context;
+        public IRepository<Category> categories { get; private set; }
+
+        public IRepository<Employee> employees { get; private set; }
+
+        public IRepository<Item> items { get; private set; }
+
+        public int CommitChanges()
+        {
+            return _context.SaveChanges();
+        }
+        //clear memory
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
